2442b547e04e805762f3a173b0f7eb81b14d08d1
c0ckt0il <161923732+c0ckt0il@users.noreply.github.com>
2025-05-26 08:36:15 +0900

    Update DemoApplication.java
    add controller for get hostname

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        String hostname = System.getenv("HOSTNAME");
+        model.addAttribute("hostname", hostname);
+        return "index"; // /WEB-INF/index.jsp
     }
 
 }
