ec28e8673298f01d6d18a53c08233cf7e4645b41
c0ckt0il <161923732+c0ckt0il@users.noreply.github.com>
2025-05-29 14:14:19 +0900

    Update DemoApplication.java
    import

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @SpringBootApplication
 @Controller
